a62a83fa05555d4e931ca40703204dabf9780739
Douglas Aguiar <douglas.aguiar@mfrural.com.br>
2024-07-18 10:48:45 -0300

    Exemplificando o uso do Yield Part 1

---
+++ YieldAula/Solution/Carro.cs
@@ -0,0 +1,18 @@
+﻿namespace Solution
+{
+    public class Carro
+    {
+        public Carro(Guid id, int numeroDeSerie)
+        {
+            Id = id;
+            NumeroDeSerie = numeroDeSerie;
+            Console.WriteLine($"Novo Carro Gerado! NumeroDeSerie: {NumeroDeSerie}");
+        }
+
+        public Guid Id { get; private set; }
+
+        public int NumeroDeSerie { get; private set; }
+
+        public override string ToString() => $"Carro Id: {Id} e NumeroDeSerie: {NumeroDeSerie}";
+    }
+}
---
+++ YieldAula/Solution/Program.cs
@@ -0,0 +1,35 @@
+﻿using Solution;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        var carros = GeradorDeCarro();
+
+        foreach (var carro in carros)
+        {
+            //Usando apenas os carros com NumeroDeSerie menor que 5
+            if (carro.NumeroDeSerie == 5)
+            {
+                break;
+            }
+
+            Console.WriteLine(carro);
+        }
+    }
+
+    //Criando 100 objetos do tipo Carro
+    public static IEnumerable<Carro> GeradorDeCarro()
+    {
+        var carros = new List<Carro>();
+
+        for (var i = 1; i <= 100; i++)
+        {
+            carros.Add(new Carro(Guid.NewGuid(), i));
+        }
+
+        Console.WriteLine("\r\n");
+
+        return carros;
+    }
+}
